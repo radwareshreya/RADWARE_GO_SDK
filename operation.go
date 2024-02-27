5b85bfa5f2b57a02fbfa064c331702f7e91ee7e7
radwareshreya <158580166+radwareshreya@users.noreply.github.com>
2024-02-27 17:09:40 +0530

    Create operation.go

---
+++ operation.go
@@ -0,0 +1,17 @@
+package RadwareGoSDK
+
+func (new_client *New_Client) CreateItem(API string, Data map[string]interface{}, additional_header map[string]string) (int, string, error) {
+	return(new_client.Request("POST", API, Data, additional_header))
+}
+
+func (new_client *New_Client) GetItem(API string, Data map[string]interface{}, additional_header map[string]string) (int, string, error) {
+	return(new_client.Request("GET", API, Data, additional_header))
+}
+
+func (new_client *New_Client) UpdateItem(API string, Data map[string]interface{}, additional_header map[string]string) (int, string, error) {
+	return(new_client.Request("PUT", API, Data, additional_header))
+}
+
+func (new_client *New_Client) DeleteItem(API string, Data map[string]interface{}, additional_header map[string]string) (int, string, error) {
+	return(new_client.Request("DELETE", API, Data, additional_header))
+}
