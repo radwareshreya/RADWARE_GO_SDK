835b8a11705eaca4b0594f1de9a9d97a7a7a792f
radwareshreya <158580166+radwareshreya@users.noreply.github.com>
2024-03-22 18:15:07 +0530

    Update actions.go

--- actions.go
+++ actions.go
@@ -16,6 +16,6 @@ func (new_client *New_Client) DeleteItem(API string, Data map[string]interface{}
 	return(new_client.Request("DELETE", API, Data, additional_header))
 }
 
-func (new_client *New_Client) CreateItemString(API string, Data []byte, additional_header map[string]string) (int, string, error) {
+func (new_client *New_Client) CreateItemString(API string, Data string, additional_header map[string]string) (int, string, error) {
 	return(new_client.Request("POST", API, Data, additional_header))
 }
