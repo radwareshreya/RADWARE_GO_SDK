14ae3aba7d368b834a74215380022e236be5aa18
radwareshreya <158580166+radwareshreya@users.noreply.github.com>
2024-04-12 11:17:47 +0530

    Update actions.go

--- actions.go
+++ actions.go
@@ -1,18 +1,18 @@
 package RadwareGoSDK
 
-func (new_client *New_Client) CreateItem(API string, Data map[string]interface{}, additional_header map[string]string) (int, string, error) {
+func (new_client *New_Client) CreateItem(API string, Data []byte, additional_header map[string]string) (int, string, error) {
 	return(new_client.Request("POST", API, Data, additional_header))
 }
 
-func (new_client *New_Client) GetItem(API string, Data map[string]interface{}, additional_header map[string]string) (int, string, error) {
+func (new_client *New_Client) GetItem(API string, Data []byte, additional_header map[string]string) (int, string, error) {
 	return(new_client.Request("GET", API, Data, additional_header))
 }
 
-func (new_client *New_Client) UpdateItem(API string, Data map[string]interface{}, additional_header map[string]string) (int, string, error) {
+func (new_client *New_Client) UpdateItem(API string, Data []byte, additional_header map[string]string) (int, string, error) {
 	return(new_client.Request("PUT", API, Data, additional_header))
 }
 
-func (new_client *New_Client) DeleteItem(API string, Data map[string]interface{}, additional_header map[string]string) (int, string, error) {
+func (new_client *New_Client) DeleteItem(API string, Data []byte, additional_header map[string]string) (int, string, error) {
 	return(new_client.Request("DELETE", API, Data, additional_header))
 }
 
